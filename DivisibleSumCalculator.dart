54d593dd24e669986f4855ee2e7276027fa3bb79
Evelyn Cleto <67518341+EvelynCleto@users.noreply.github.com>
2023-11-17 18:22:23 -0300

    Update DivisibleSumCalculator.dart

--- DivisibleSumCalculator.dart
+++ DivisibleSumCalculator.dart
@@ -1,17 +1,19 @@
 import 'dart:io'; // Importa a biblioteca de I/O do Dart
 
 // Função para calcular a soma dos números divisíveis por 3 ou 5
-int somatorio(int n) {
+Map<String, dynamic> somatorio(int n) {
     int soma = 0; // Inicializa a variável soma
+    List<int> numeros = []; // Inicializa a lista de números
     // Loop de 0 até n
     for(int i = 0; i < n; i++) {
-        // Se o número for divisível por 3 ou 5, adicione-o à soma
+        // Se o número for divisível por 3 ou 5, adicione-o à soma e à lista de números
         if(i % 3 == 0 || i % 5 == 0) {
             soma += i;
+            numeros.add(i);
         }
     }
-    // Retorne a soma
-    return soma;
+    // Retorne a soma e a lista de números
+    return {'soma': soma, 'numeros': numeros};
 }
 
 // Função para verificar se o input é um número inteiro
@@ -32,8 +34,10 @@ void main() {
         // Se o número for um inteiro positivo, calcule a soma
         if(isInt(numStr)) {
             int num = int.parse(numStr);
-            // Exibe a soma
-            print("Soma: ${somatorio(num)}");
+            // Calcula a soma e obtém a lista de números
+            Map<String, dynamic> resultado = somatorio(num);
+            // Exibe a soma e a lista de números
+            print("Soma: ${resultado['soma']}. Resultante do somatório dos números ${resultado['numeros'].join(', ')} que são menores que $num.");
             // Pergunta ao usuário se ele deseja continuar
             print("Continuar? (S/N)");
             String continuar = stdin.readLineSync();
